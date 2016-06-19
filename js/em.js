8ba008456059d0d6bfc48f7303ae310b15c430c1
Dustin John Pfister <dustin.pfister@gmail.com>
2016-06-19 14:57:50 -0400

    inMaster updated to handle position of canvas better

--- js/em.js
+++ js/em.js
@@ -50,27 +50,45 @@ var EM = (function(){
 		
 		inMaster : function(e){
 		
-		    var box = e.target.getBoundingClientRect();
+		    var box = e.target.getBoundingClientRect(),
+			x,y;
 		
 		    e.preventDefault();
 	
+	        if(e.touches){
+	
+	            x = e.touches[0].clientX;
+				y = e.touches[0].clientY;
+	
+	        }else{
+				
+				x = e.clientX;
+				y = e.clientY;
+	
+	        }
+			
+			EM.ME.cx = x - EM.ME.w / 2 - box.left;
+			EM.ME.cy = y - EM.ME.h / 2 - box.top;
+	
+	/*
 	        if(e.touches){
 	
 	            console.log('touch');
 				
 				console.log(e.touches[0].clientX);
 	
-	            EM.ME.cx = e.touches[0].clientX;
-				EM.ME.cy = e.touches[0].clientY;
+	            EM.ME.cx = e.touches[0].clientX - EM.ME.w / 2;
+				EM.ME.cy = e.touches[0].clientY - EM.ME.h / 2;
 	
 	        }else{
 	
 	            console.log('mouse');
 				
-				EM.ME.cx = e.clientX;
-				EM.ME.cy = e.clientY;
+				EM.ME.cx = e.clientX - EM.ME.w / 2;;
+				EM.ME.cy = e.clientY - EM.ME.h / 2;
 	
 	        }
+		*/
 		
 		}
 		
