8aa3fe15f081aa0acf9241660a5e15dc184325ba
Dustin John Pfister <dustin.pfister@gmail.com>
2016-06-19 18:45:13 -0400

    moving to center

--- js/em.js
+++ js/em.js
@@ -33,6 +33,12 @@ var EM = (function(){
 	
 	},
 	
+	distance = function(x1,y1,x2,y2){
+		
+		return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+		
+	},
+	
 	Orbit = function(){
 	
 	    this.x = 0;
@@ -51,7 +57,9 @@ var EM = (function(){
 		    //this.ME.cx += this.ME.dx;
 			//this.ME.cy += this.ME.dy;
 		
-		    var now = new Date();
+		    var now = new Date(), 
+			aToCenter = Math.atan2(ME.cy - ME.y, ME.cx - ME.x),
+			dToCenter = distance(ME.x,ME.y,ME.cx,ME.cy), a, d;
 			
 			// if the amount of time that has passed sense the last touch is greater then touchLife
 			if(now - ME.lastTouch >= ME.touchLife){
@@ -71,21 +79,35 @@ var EM = (function(){
 				// delta happy default
 				ME.deltaHappy = -0.01;
 				
+				// if being touched
 				if(ME.touchArray.length > 0){
 					
 				   ME.deltaHappy = ME.touchArray.length / ME.maxTouch * 0.05;	
 					
-				   ME.x = ME.touchArray[0].x - ME.w / 2;
-				   ME.y = ME.touchArray[0].y - ME.h / 2;
+				   //ME.x = ME.touchArray[0].x - ME.w / 2;
+				   //ME.y = ME.touchArray[0].y - ME.h / 2;
 					
+                   //ME.x += 5;
+				   
+				// no touching
 				}else{
 					
-					ME.x = ME.cx;
-					ME.y = ME.cy;
+					//ME.x = ME.cx;
+					//ME.y = ME.cy;
+					
+					//console.log(ME.cx - ME.x);
+					
+					// find angle and dustance to center
+					/*
+					var aToCenter = Math.atan2(ME.cy - ME.y, ME.cx - ME.x),
+					dToCenter = distance(ME.x,ME.y,ME.cx,ME.cy);
+					
+				    ME.dx = Math.cos(aToCenter) * (dToCenter);
+					ME.dy = Math.sin(aToCenter) * (dToCenter);
+					*/
+					
 					
 				}
-				
-				console.log(ME.happy);
 				
 				ME.happy += ME.deltaHappy;
 				if(ME.happy < 0){ ME.happy = 0; }
@@ -96,6 +118,21 @@ var EM = (function(){
 				
 				
 			}
+			
+			// update on each frame tick:
+			
+			// if no touching move to center 
+			if(ME.touchArray.length === 0){
+			
+		    	ME.dx = Math.cos(aToCenter) * (dToCenter / 10);
+		    	ME.dy = Math.sin(aToCenter) * (dToCenter / 10);
+			
+			}else{
+				
+			}
+			
+			ME.x += ME.dx;
+			ME.y += ME.dy;
 		
 		
 		},
@@ -131,6 +168,9 @@ var EM = (function(){
 			
 			ME.cx = ME.xMax / 2 - ME.w / 2;
 			ME.cy = ME.yMax / 2 - ME.h / 2;
+			
+			ME.x = ME.cx + 100;
+			ME.y = ME.cy;
 			
 		},
 		
