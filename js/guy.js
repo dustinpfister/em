d80c6650f0c63fc65c725df5afc5ec17b3c3c331
Dustin John Pfister <dustin.pfister@gmail.com>
2016-06-29 18:29:34 -0400

    guy moves to target

--- js/guy.js
+++ js/guy.js
@@ -3,16 +3,52 @@ var guy = (function(){
 	
 	var state = {
 		
-		homeX : playground.cx,
-		homeY : playground.cy,
+		homeX : playground.pg.cx,
+		homeY : playground.pg.cy,
+		targetX : playground.pg.cx,
+		targetY : playground.pg.cy,
 		x: 100,
-		y: 100
+		y: 100,
+		dx: 0,
+		dy: 0,
+		moveRate: 5
 		
-	};
+	},
 	
-	var api = {
+	// ALERT! this is also in playground!
+	distance = function(x1,y1,x2,y2){
 		
-		state: state
+		return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+		
+	},
+	
+	api = {
+		
+		state: state,
+		
+		update : function(){
+			
+			var a = Math.atan2(state.targetY - state.y, state.targetX - state.x),
+			d = distance(state.x,state.y,state.targetX,state.targetY);
+			
+			//state.dx = Math.cos(angle) * 5;
+			//state.dy = Math.sin(angle) * 5;
+			
+			state.dx = Math.cos(a) * (d / state.moveRate);
+		    state.dy = Math.sin(a) * (d / state.moveRate);
+			
+			state.x += state.dx;
+			state.y += state.dy;
+			
+		},
+		
+		resize : function(){
+			
+			state.homeX = playground.pg.cx;
+			state.homeY = playground.pg.cy;
+			state.targetX = playground.pg.cx;
+			state.targetY = playground.pg.cy;
+		}
 		
 	};
 	
